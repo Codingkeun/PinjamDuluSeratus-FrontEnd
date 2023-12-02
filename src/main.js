314df5ec80a67c70a77a8f56e387336e78cc5e96
Cecep Rokani <cecepfahriazal1997@gmail.com>
2023-12-03 00:36:54 +0700

    Add new package

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// load utils
 import router from './router'
 import store from './store'
 
+// load external library
+import VueCountdown from '@chenfengyuan/vue-countdown';
+
+// load assets
 import './assets/css/bootstrap.css'
 import './assets/css/style.css'
 import './assets/css/responsive.css'
 
 const app = createApp(App)
+
 app.use(router)
 app.use(store)
+
+app.component(VueCountdown.name, VueCountdown);
+
 app.mount('#app')
