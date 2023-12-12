c65546a5be6d5f1755b9de5b75e0e4e836d94ca7
rianaratna <riana06huan18@gmail.com>
2023-12-12 13:30:26 +0700

    Add: shadow on navigation when window is on scroll

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,20 @@ import Footer from './components/Footer.vue'
     <Footer v-if="$route.meta.footer"></Footer>
   </div>
 </template>
+
+<script>
+import $ from 'jquery';
+export default {
+  mounted() {
+    $(window).on('scroll', () => {
+      const mainBottomNav = $('.container .navbar').height();
+      const stop = Math.round($(window).scrollTop());
+      if (stop > mainBottomNav) {
+          $('.container').addClass('scroll');
+      } else {
+          $('.container').removeClass('scroll');
+      }
+    })
+  }
+}
+</script>
