721ba523c50b36450e808349c33176efa5a52fc5
rianaratna <riana06huan18@gmail.com>
2023-12-17 15:27:06 +0700

    Feat: implement APIs list active investment

--- src/views/investor/investment/Investment.vue
+++ src/views/investor/investment/Investment.vue
@@ -2,39 +2,70 @@
 
     <main class="mt-5 mb-5 w-100 d-flex flex-column" style="padding-inline: calc(15% / 2);">
         <h1 class="font-weight-semibold">Daftar Peminjam Aktif</h1>
+        <div class="d-flex justify-content-end">
+            <div class="mb-3 text-muted" v-if="pagination.total">Menampilkan {{pagination.total}} data</div>
+        </div>
         <div class="active-lendees-list d-flex flex-column mt-4" style="gap: 3rem">
-            <div class="card p-5 rounded-10 container-card-shadow" v-for="item in [1,2,3,4,5,6,7,8]">
-                <div class="card-body p-0 mb-4">
-                    <h2 class="card-title font-weight-bold m-0 par-1-5em" id="activeLendeesLoanAmount">Rp 2,000,000</h2>
-                    <h3 class="card-subtitle font-weight-semibold text-primary m-0 mb-4 par-1em" id="activeLendeesTipAmount">Tip Rp 500,000</h3>
+            <div class="card p-5 rounded-10 container-card-shadow" v-if="list.length">
+                <div class="card-body p-0 mb-4" v-for="item in list">
+                    <h2 class="card-title font-weight-bold m-0 par-1-5em" id="activeLendeesLoanAmount">Rp {{ $toCurrency(item.nominal) }}</h2>
+                    <h3 class="card-subtitle font-weight-semibold text-primary m-0 mb-4 par-1em" id="activeLendeesTipAmount">Tip Rp {{ $toCurrency((item.tip)) }}</h3>
                     <div class="d-flex flex-column">
-                        <h3 class="card-text font-weight-semibold par-1-2em" id="activeLendeesName">Andrea</h3>
-                        <h4 class="card-text par-1em"><span id="activeLendeesNPM">NPM 1019201923</span></h4>
-                        <h4 class="card-text par-1em"><span id="activeLendeesPhone">08123456789</span></h4>
+                        <h3 class="card-text font-weight-semibold par-1-2em" id="activeLendeesName">{{item.peminjam_name || '-'}}</h3>
+                        <h4 class="card-text par-1em"><span id="activeLendeesNPM">NPM {{item.peminjam_npm || '-'}}</span></h4>
+                        <h4 class="card-text par-1em"><span id="activeLendeesPhone">{{item.peminjam_phone || '-'}}</span></h4>
                     </div>
                 </div>
                 <button @click="$router.push('investment/detail/' + item)" class="btn btn-primary font-weight-semibold">Detail</button>
             </div>
+            <div v-else>
+                <span class="text-center text-muted">Belum ada pengajuan pinjaman</span>
+            </div>
         </div>
 
-        <nav class="mt-5 align-self-center">
-            <ul class="pagination">
-                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
-        </nav>
+        <div class="d-flex justify-content-center" v-show="list.length">
+            <Pagination :page="pagination.page" :prev="pagination.prev" :next="pagination.next" v-on:fetchData="fetchData"></Pagination>
+        </div>
     </main>
 </template>
 <script>
+import apiEnpoint from '@/services/api-endpoint'
+import { ApiCore } from '@/services/core'
+
 export default {
     name: 'Investment',
     data() {
         return {
-            
+            list: [],
+            pagination: {
+                prev: false,
+                next: false,
+                page: 1,
+                limit: 5,
+                total: 0
+            },
         }
+    },
+    mounted() {
+        this.fetchData(this.pagination.page)
+    },
+    methods: {
+        fetchData(page) {
+            this.list = []
+            ApiCore.get(`${apiEnpoint.INVESTMENT}/pinjaman-aktif`, {
+                page: page,
+                limit: this.pagination.limit,
+                status_approval: 'wait'
+            }).then((result) => {
+                if (result.status) {
+                    this.list = result.data
+                }s
+                this.pagination.prev = result.pagination.prev
+                this.pagination.next = result.pagination.next
+                this.pagination.page = result.pagination.page
+                this.pagination.total = result.pagination.total
+            })
+        },
     }
 }
 </script>
