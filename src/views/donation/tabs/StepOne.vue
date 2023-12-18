21adf53de1e69399a4b8e600a01d91df81265641
rianaratna <riana06huan18@gmail.com>
2023-12-18 16:45:38 +0700

    Fix: update donation

--- src/views/donation/tabs/StepOne.vue
+++ src/views/donation/tabs/StepOne.vue
@@ -1,81 +1,89 @@
 <template>
     <div class="donation-nominal-area mt-4 mb-5">
-        <form class="d-flex flex-column" style="gap: 3rem;" @submit.prevent="submit">
+        <Form :validation-schema="schemaNext" class="d-flex flex-column" style="gap: 3rem;" @submit.prevent="submit">
             <div class="w-100 d-flex flex-column" style="padding-inline: calc(15% / 2); gap: 1rem;">
                 <label for="donationAmount" class="text-center font-weight-semibold" aria-hidden="true">Masukkan jumlah donasi</label>
+                <Field type="text" v-model="form.nominal" name="nominal" class="d-none" />
                 <div class="input-group d-flex mx-auto">
                     <span class="input-group-text border-right-0" style="border-top-right-radius: 0; border-bottom-right-radius: 0;">Rp</span>
-                    <input type="text" placeholder="Masukkan jumlah donasi" class="form-control" id="donationAmount" required>
-                    <div class="amountInvalidInputMessage flex-wrap w-100" hidden>
-                        <p class="text-danger font-weight-medium mb-0 par-0-8em">
-                            Inputan harus berupa angka dengan minimal nominal adalah Rp 10,000
-                        </p>
-                    </div>
+                    <input v-maska 
+                        placeholder="Masukkan jumlah donasi"
+                        class="form-control"
+                        id="donationAmount" 
+                        @maska="setNominal"
+                        data-maska="9,99#"
+                        data-maska-tokens="9:[0-9]:repeated"
+                        data-maska-reversed/>
                 </div>
+                <ErrorMessage name="nominal" :class="'text-center text-danger'" />
             </div>
             <div class="d-flex flex-column w-100" style="gap: 1rem;">
                 <h1 class="font-weight-semibold text-center par-1em" tabindex="0">Pilih metode pembayaran</h1>
                 <div class="d-flex card-horizontal-scroll" style="gap: 2rem; padding-inline: calc(15% / 2);">
-                    <div class="item-card-shadow rounded-10 px-3 py-2" style="min-width: 15rem; min-height: 8rem;">
-                        <input type="radio" name="paymentMethod" id="donationPaymentMethod_bca" required>
-                        <label for="donationPaymentMethod_bca" class="position-relative d-flex justify-content-center" aria-label="Bank BCA">
-                            <img src="@/assets/images/bank/BCA.svg" alt="" class="p-3">
-                        </label>
-                    </div>
-                    <div class="item-card-shadow rounded-10 px-3 py-2" style="min-width: 15rem; min-height: 8rem">
-                        <input type="radio" name="paymentMethod" id="donationPaymentMethod_bni" required>
-                        <label for="donationPaymentMethod_bni" class="position-relative d-flex justify-content-center" aria-label="Bank BNI">
-                            <img src="@/assets/images/bank/BNI.svg" alt="" class="p-3">
-                        </label>
-                    </div>
-                    <div class="item-card-shadow rounded-10 px-3 py-2" style="min-width: 15rem; min-height: 8rem">
-                        <input type="radio" name="paymentMethod" id="donationPaymentMethod_bniSyarih" required>
-                        <label for="donationPaymentMethod_bniSyarih" class="position-relative d-flex justify-content-center" aria-label="Bank BNI Syariah">
-                            <img src="@/assets/images/bank/BNI_Syariah.svg" alt="" class="p-3">
-                        </label>
-                    </div>
-                    <div class="item-card-shadow rounded-10 px-3 py-2" style="min-width: 15rem; min-height: 8rem">
-                        <input type="radio" name="paymentMethod" id="donationPaymentMethod_bri" required>
-                        <label for="donationPaymentMethod_bri" class="position-relative d-flex justify-content-center" aria-label="Bank BRI">
-                            <img src="@/assets/images/bank/BRI.svg" alt="" class="p-3">
-                        </label>
-                    </div>
-                    <div class="item-card-shadow rounded-10 px-3 py-2" style="min-width: 15rem; min-height: 8rem">
-                        <input type="radio" name="paymentMethod" id="donationPaymentMethod_mandiri" required>
-                        <label for="donationPaymentMethod_mandiri" class="position-relative d-flex justify-content-center" aria-label="Bank Mandiri">
-                            <img src="@/assets/images/bank/Mandiri.svg" alt="" class="p-1">
-                        </label>
-                    </div>
-                    <div class="item-card-shadow rounded-10 px-3 py-2" style="min-width: 15rem; min-height: 8rem">
-                        <input type="radio" name="paymentMethod" id="donationPaymentMethod_visa" required>
-                        <label for="donationPaymentMethod_visa" class="position-relative d-flex justify-content-center" aria-label="VISA">
-                            <img src="@/assets/images/bank/VISA.svg" alt="" class="p-3">
-                        </label>
-                    </div>
-                    <div class="item-card-shadow rounded-10 px-3 py-2" style="min-width: 15rem; min-height: 8rem">
-                        <input type="radio" name="paymentMethod" id="donationPaymentMethod_masterCard" required>
-                        <label for="donationPaymentMethod_masterCard" class="position-relative d-flex justify-content-center" aria-label="Mastercard">
-                            <img src="@/assets/images/bank/MasterCard.svg" alt="" class="">
+                    <div class="item-card-shadow rounded-10 px-3 py-2" style="min-width: 15rem; min-height: 8rem;" v-for="bank, index in listBank" :key="index">
+                        <Field type="radio" name="paymentMethod" :id="`bank_${index}`" :value="bank.bank_name" v-model="form.bank"/>
+                        <label :for="`bank_${index}`" class="position-relative d-flex justify-content-center" :aria-label="`Bank ${index}`">
+                            <img :src="bank.bank_logo" :alt="`Bank ${bank.bank_name}`" class="p-3">
                         </label>
                     </div>
                 </div>
+                <ErrorMessage name="bank" :class="'text-danger'" style="padding-inline:calc(15% / 2)" />
             </div>
             <button type="submit" class="btn btn-primary font-weight-semibold w-75 mx-auto">Lanjut</button>
-        </form>
+        </Form>
     </div>
 </template>
 <script>
+import { vMaska } from "maska"
+
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import {string, number, object, array} from 'yup';
+
+import { ApiCore } from '@/services/core';
+import apiEnpoint from '@/services/api-endpoint';
+
 export default {
     name: 'DonationStepOne',
     data() {
         return {
-            form: {}
+            form: {
+                nominal: null,
+                bank: '',
+            },
+            listBank: []
         }
+    },
+    directives: { maska: vMaska },
+    setup() {
+        const schemaNext = object({
+            nominal: number().nullable().min(10000, 'Jumlah donasi minimal Rp. 10,000.00').required('Masukan jumlah donasi'),
+            bank: string().required('Silahkan pilih bank'),
+        });
+
+        return {
+            schemaNext,
+        }
+    },
+    components: {Field, Form, ErrorMessage},
+    mounted() {
+        this.fetchPaymentMethod()
     },
     methods: {
         submit() {
             this.$emit("onHandleSubmit", {form: this.form});
-        }
+        },
+        setNominal(data) {
+            this.form.nominal=data.detail.unmasked
+        },
+        fetchPaymentMethod() {
+            ApiCore.get('payment-method').then((data) => {
+                if (data.status)
+                    this.listBank = data.data
+                this.listBank.forEach(element => {
+                    element.bank_logo = this.$getUrlImageBank(element.bank_logo)
+                });
+            }).catch((error) => {})
+        },
     }
 }
 </script>
--- src/views/donation/tabs/StepTwo.vue
+++ src/views/donation/tabs/StepTwo.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="donation-nominal-area mt-4 mb-5" style="padding-inline: calc(15% / 2);">
-        <form class="d-flex flex-column" style="gap: 3rem;" @submit.prevent="submit">
+        <Form :validation-schema="schemaSubmit" class="d-flex flex-column" style="gap: 3rem;" @submit="handleSubmit">
             <h1 for="donationAmount" class="text-center font-weight-semibold m-0 par-1em">Masukkan identitas diri</h1>
             <div class="d-flex flex-column form-identity p-5 rounded-10 container-card-shadow" style="gap: 2rem; padding-inline: calc(15% / 2);">
                 <div class="">
                     <label for="name">Nama</label>
-                    <input type="text" id="name" class="form-control" placeholder="Masukkan nama" required>
+                    <Field v-model="form.name" name="name" type="text" id="name" class="form-control" placeholder="Masukkan nama" />
+                    <ErrorMessage name="name" :class="'text-center text-danger'" />
                 </div>
                 <div class="">
                     <label for="email">Email</label>
-                    <input type="email" id="email" class="form-control" placeholder="Masukkan email" required>
+                    <Field v-model="form.email" name="email" id="email" class="form-control" placeholder="Masukkan email" />
+                    <ErrorMessage name="email" :class="'text-center text-danger'" />
                 </div>
                 <div class="">
                     <label for="phone">Nomor HP</label>
-                    <input type="text" id="phone" class="form-control" placeholder="Masukkan nomor HP" required>
+                    <Field v-model="form.phone" name="phone" type="text" id="phone" class="form-control" placeholder="Masukkan nomor HP" />
+                    <ErrorMessage name="phone" :class="'text-center text-danger'" />
                 </div>
             </div>
             <div class="button-group d-flex flex-column" style="gap: 2rem;">
@@ -24,17 +27,61 @@
     </div>
 </template>
 <script>
+import { vMaska } from "maska"
+
+import { Field, Form, ErrorMessage } from 'vee-validate';
+import {string, number, object, array} from 'yup';
+
+import { ApiCore } from '@/services/core';
+import apiEnpoint from '@/services/api-endpoint';
+
 export default {
     name: 'DonationStepTwo',
     data() {
         return {
-            form: {}
+            form: {
+                name: '',
+                email: '',
+                phone: '',
+            }
         }
     },
+    directives: { maska: vMaska },
+    setup() {
+        const schemaSubmit = object({
+            name: string().required('Masukan nama'),
+            email: string().email('Masukan email yang valid').required('Masukan email'),
+            phone: string().required('Masukan nomor handphone'),
+        });
+
+        return {
+            schemaSubmit,
+        }
+    },
+    components: {Field, Form, ErrorMessage},
     methods: {
         submit() {
             this.$emit("onHandleSubmit", {form: this.form});
-        }
+        },
+        async handleSubmit() {
+            try {
+                this.form.fetch = true
+                const process = await ApiCore.store(`/donasi`, {
+                    ...this.form
+                })
+                this.form.fetch = false
+                if (process.status) {
+                    this.$toast.success(process.message);
+                    this.$router.push({ name: 'loan' })
+                    submit();
+                } else {
+                    this.$toast.error(process.message);
+                }
+            } catch(error) {
+                this.form.fetch = false
+                this.$toast.error(error);
+            }
+        },
     }
 }
 </script>
